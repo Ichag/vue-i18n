545dafdc64634872d7ad60218df7b29b2caa443f
Max Hellwig <mail@maxhellwig.de>
2018-07-30 09:18:19 +0200

    Add check if $i18n is already defined on prototype
    An error message is thrown if the property '$i18n' is already defined on the Vue prototype. This Commit will hopefully prevents this.
    Bug report:
    https://github.com/kazupon/vue-i18n/issues/402

--- src/extend.js
+++ src/extend.js
@@ -2,9 +2,13 @@
 
 export default function extend (Vue: any): void {
   // $FlowFixMe
-  Object.defineProperty(Vue.prototype, '$i18n', {
-    get () { return this._i18n }
-  })
+  if (!Vue.prototype.hasOwnProperty('$i18n') {
+    Object.defineProperty(Vue.prototype, '$i18n', {
+      get () { return this._i18n }
+    })
+  } else {
+  console.log('already installed')
+}
 
   Vue.prototype.$t = function (key: Path, ...values: any): TranslateResult {
     const i18n = this.$i18n
